17c6afb8f28148ab65b8ad93a30fa48529db6ea0
esdragor <64788862+esdragor@users.noreply.github.com>
2023-11-20 16:51:10 +0100

    refacto input move

--- Assets/Scripts/PlayerEntity.cs
+++ Assets/Scripts/PlayerEntity.cs
@@ -4,53 +4,72 @@ using UnityEngine;
 public class PlayerEntity : Entity
 {
     Vector3 newPosition;
+    
+    private void Start()
+    {
+        newPosition = transform.position;
+    }
 
     private void LateUpdate()
     {
         var position = transform.position;
-        switch (ForwardDirection) // a refacto vite !
+
+        switch (ForwardDirection)
         {
-            case Direction.Up:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y + 1), 0);
-                //transform.DOMoveY(Mathf.RoundToInt(transform.position.y + 1), delayToMove);
+            case Direction.Up or Direction.UpLeft or Direction.UpRight:
+                newPosition.y = Mathf.RoundToInt(position.y + 1);
                 break;
-            case Direction.Down:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x),
-                    Mathf.RoundToInt(position.y - 1), 0);
-                //transform.DOMoveY(Mathf.RoundToInt(transform.position.y - 1), delayToMove);
+            case Direction.Down or Direction.DownLeft or Direction.DownRight:
+                newPosition.y = Mathf.RoundToInt(position.y - 1);
                 break;
-            case Direction.Left:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x - 1),
-                    Mathf.RoundToInt(position.y), 0);
-                //transform.DOMoveX(Mathf.RoundToInt(transform.position.x - 1), delayToMove);
+            case Direction.Left or Direction.DownLeft or Direction.UpLeft:
+                newPosition.x = Mathf.RoundToInt(position.x - 1);
                 break;
-            case Direction.Right:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x + 1),
-                    Mathf.RoundToInt(position.y), 0);
-                //transform.DOMoveX(Mathf.RoundToInt(transform.position.x + 1), delayToMove);
-                break;
-            case Direction.DownLeft:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x - 1),
-                    Mathf.RoundToInt(position.y - 1), 0);
-                //transform.DOMove(newPosition, delayToMove);
-                break;
-            case Direction.DownRight:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x + 1),
-                    Mathf.RoundToInt(position.y - 1), 0);
-                break;
-            case Direction.UpLeft:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x - 1),
-                    Mathf.RoundToInt(position.y + 1), 0);
-                break;
-            case Direction.UpRight:
-                newPosition = new Vector3(Mathf.RoundToInt(position.x + 1),
-                    Mathf.RoundToInt(position.y + 1), 0);
-                break;
-            case Direction.None:
-                //transform.DOComplete();
-                //transform.position = newPosition;
+            case Direction.Right or Direction.DownRight or Direction.UpRight:
+                newPosition.x = Mathf.RoundToInt(position.x + 1);
                 break;
         }
+
+
+            // switch (ForwardDirection) // a refacto vite !
+            // {
+            //     case Direction.Up or D:
+            //         newPosition = new Vector3(Mathf.RoundToInt(newPosition.x), Mathf.RoundToInt(position.y + 1), 0);
+            //         break;
+            //     case Direction.Down:
+            //         newPosition = new Vector3(Mathf.RoundToInt(position.x),
+            //             Mathf.RoundToInt(position.y - 1), 0);
+            //         break;
+            //     case Direction.Left:
+            //         newPosition = new Vector3(Mathf.RoundToInt(position.x - 1),
+            //             Mathf.RoundToInt(position.y), 0);
+            //         break;
+            //     case Direction.Right:
+            //         newPosition = new Vector3(Mathf.RoundToInt(position.x + 1),
+            //             Mathf.RoundToInt(position.y), 0);
+            //         break;
+            //     case Direction.DownLeft:
+            //         newPosition = new Vector3(Mathf.RoundToInt(position.x - 1),
+            //             Mathf.RoundToInt(position.y - 1), 0);
+            //         //transform.DOMove(newPosition, delayToMove);
+            //         break;
+            //     case Direction.DownRight:
+            //         newPosition = new Vector3(Mathf.RoundToInt(position.x + 1),
+            //             Mathf.RoundToInt(position.y - 1), 0);
+            //         break;
+            //     case Direction.UpLeft:
+            //         newPosition = new Vector3(Mathf.RoundToInt(position.x - 1),
+            //             Mathf.RoundToInt(position.y + 1), 0);
+            //         break;
+            //     case Direction.UpRight:
+            //         newPosition = new Vector3(Mathf.RoundToInt(position.x + 1),
+            //             Mathf.RoundToInt(position.y + 1), 0);
+            //         break;
+            //     case Direction.None:
+            //         //transform.DOComplete();
+            //         //transform.position = newPosition;
+            //         break;
+            // }
 
         if (ForwardDirection != Direction.None)
         {
