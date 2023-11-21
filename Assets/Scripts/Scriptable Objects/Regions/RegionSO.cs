1dabc34cb4f32d9867e3e9ad9512b9a6f98d8cbe
Arthur <35684550+Daedras10@users.noreply.github.com>
2023-11-21 10:52:46 +0100

    Region early work

--- Assets/Scripts/Scriptable Objects/Regions/RegionSO.cs
+++ Assets/Scripts/Scriptable Objects/Regions/RegionSO.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+using Scriptable_Objects.Encounters;
 using UnityEngine;
 
 namespace Scriptable_Objects.Regions
 {
-    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    [CreateAssetMenu(fileName = "new Region", menuName = "ScriptableObjects/Region", order = 0)]
     public class RegionSO : ScriptableObject
     {
+        [field:Header("---RegionSO---")]
+        [field:SerializeField] public string Name { get; private set; }
+        
+        [field:SerializeField] public List<EncounterTableSO> EncounterTables { get; private set; }
+
+        [field:Header("Audio")]
+        [field:SerializeField] public List<AudioClip> Ambiance { get; private set; }
+        [field:SerializeField] public List<AudioClip> BattleSounds { get; private set; }
         
     }
 }
